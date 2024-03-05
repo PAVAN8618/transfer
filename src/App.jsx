bc67ae612ddf9e0b2d916b23882e8ae29d52146a
Pavan.Kumar <pnttiwari99@gmail.com>
2024-03-05 23:21:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='main'>
+
+    <div className='transfer'>
+      <input type='checkbox' id='India'/>
+      <label htmlFor="India">India</label>
+      <input type='checkbox' id='Pakistan'/>
+      <label htmlFor="Pakistan">Pakistan</label>
+      <input type='checkbox' id='USA'/>
+      <label htmlFor="USA">USA</label>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default App
